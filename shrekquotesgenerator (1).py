00710fe11f4602353a0e29a4f196819d1917e009
KKNINU <32983082+KKNINU@users.noreply.github.com>
2023-04-04 23:04:51 +1000

    Add files via upload

---
+++ shrekquotesgenerator (1).py
@@ -0,0 +1,23 @@
+#get out mi swamp
+
+quotein = input("Welcome to Shrek Quote Central, Which character matches your mood today? ")
+
+if quotein == 'Shrek':
+    print("Ogres are like onions. “Onions have layers. Ogres have layers")
+    
+elif quotein == 'Fiona':
+    print ("There is an arrow in your butt.")
+        
+elif quotein == 'Donkey':
+    print ("In the morning I'm making waffles!")
+    
+elif quotein == 'Puss in Boots':
+    print ("Pray for mercy, from Puss!")
+
+elif quotein == 'Prince Charming':
+    print("Yes, Cherry Flavored. Want A Taste?")
+  
+        
+elif quotein == 'Lord Farquaad':
+    print("Some of you may die, but its a sacrifice Im willing to make")
+    
